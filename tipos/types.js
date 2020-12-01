b05eb3ed230384f0a3ae019e998e06a21d4d7b28
jpablolima <jp.pablolima@gmail.com>
2020-12-01 12:43:27 -0300

    types TS

--- tipos/types.js
+++ tipos/types.js
@@ -1,27 +1,27 @@
 "use strict";
 // string
-var nome = 'Pablo';
-console.log(nome);
+// const nome =  'Pablo'
+// console.log(nome);
 //number
 var idade = 29;
-console.log(idade);
+// console.log(idade);
 // boolean
 var possuiHobbies = true;
-console.log(possuiHobbies);
+// console.log(possuiHobbies);
 // Tipos explícitos
 var minhaIdade;
 minhaIdade = 29;
-console.log(minhaIdade);
+// console.log(minhaIdade);
 // array
 var hobbies = ['Esportes', 'cozinhar'];
-console.log(hobbies);
+// console.log(hobbies);
 hobbies = [100, 200];
-console.log(hobbies);
+// console.log(hobbies);
 // Tuplas
 var endereco = ['Quadra A', 20, ""];
-console.log(endereco);
+// console.log(endereco);
 endereco = ['Rua teste', 1000, 'bloco A'];
-console.log(endereco);
+//console.log(endereco);
 //Enums
 var Cor;
 (function (Cor) {
@@ -32,5 +32,80 @@ var Cor;
 })(Cor || (Cor = {}));
 var minhaCor = Cor;
 var testCor;
-console.log(minhaCor);
-console.log(Cor.Amarelo);
+// console.log(minhaCor);
+//console.log(Cor.Amarelo);
+//Any
+var carro = 'BMW';
+// console.log(carro);
+carro = { marca: "BMW", ano: 2020 };
+//console.log(carro);
+// types in functions
+function retonaMeuNome() {
+    // return nome;
+}
+// console.log(retonaMeuNome());
+function digaOi() {
+    // console.log('oi')
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+// console.log(multiplicar(4,3));
+// Functions with types
+var calculo;
+calculo = multiplicar;
+// console.log(calculo(5,6)) 
+// Object and types
+// objetos
+var usuario = {
+    name: 'Pablo',
+    age: 29
+};
+var funcionario = {
+    supervisores: ['Ana', 'Pedro'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto Normal';
+        }
+        else {
+            return 'Fora do Ponto';
+        }
+    }
+};
+// console.log(funcionario.supervisores)
+// console.log(funcionario.baterPonto(8))
+// console.log(funcionario.baterPonto(10))
+// Union Types
+var nota = 10;
+// console.log(`Nota : ${nota}`);
+// Never
+function falha(msg) {
+    throw new Error(msg);
+}
+var produto = {
+    nome: 'Carro',
+    preco: 60000,
+    validarProduto: function () {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha('Precisa de um nome');
+        }
+        if (this.preco <= 0) {
+            falha('Preço inválido');
+        }
+    }
+};
+produto.validarProduto();
+var contaBancaria = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+var correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
--- tipos/types.ts
+++ tipos/types.ts
@@ -1,11 +1,11 @@
 // string
-const nome =  'Pablo'
+// const nome =  'Pablo'
 // console.log(nome);
 
 
 //number
 const idade = 29;
-console.log(idade);
+// console.log(idade);
 
 // boolean
 let possuiHobbies = true;
@@ -19,13 +19,13 @@ minhaIdade = 29;
 
 // array
 let hobbies: any = ['Esportes', 'cozinhar']
-console.log(hobbies);
+// console.log(hobbies);
 hobbies = [100,200]
 // console.log(hobbies);
 
 // Tuplas
 let endereco : [string, number, string] = ['Quadra A', 20, ""];
-console.log(endereco);
+// console.log(endereco);
 endereco = ['Rua teste', 1000, 'bloco A'];
 //console.log(endereco);
 
@@ -40,25 +40,25 @@ enum Cor {
 let minhaCor = Cor;
 let testCor: Cor.Amarelo;
 
-console.log(minhaCor);
+// console.log(minhaCor);
 //console.log(Cor.Amarelo);
 
 //Any
 
 let carro: any = 'BMW';
-console.log(carro);
+// console.log(carro);
 carro = { marca: "BMW", ano: 2020};
 //console.log(carro);
 
 // types in functions
 
 function retonaMeuNome(): string{
-    return nome;
+    // return nome;
 }
-console.log(retonaMeuNome());
+// console.log(retonaMeuNome());
 
 function digaOi(): void{
-    console.log('oi')
+    // console.log('oi')
 }
 digaOi();
 
@@ -87,7 +87,7 @@ let usuario: { name: string, age: number} = {
 }
 // console.log(usuario);
 
-
+// Alias
 type Funcionario = {
     supervisores: string [],
     baterPonto: (horas: number) => string
@@ -104,7 +104,76 @@ let funcionario : Funcionario = {
         }
     }
 }
+// console.log(funcionario.supervisores)
+// console.log(funcionario.baterPonto(8))
+// console.log(funcionario.baterPonto(10))
 
-console.log(funcionario.supervisores)
-console.log(funcionario.baterPonto(8))
-console.log(funcionario.baterPonto(10))
+
+// Union Types
+let nota: number | string = 10;
+// console.log(`Nota : ${nota}`);
+
+// Never
+function falha(msg: string): never{
+    throw new Error(msg)
+}
+
+const produto = {
+    nome: 'Carro',
+    preco: 60000,
+    validarProduto(){
+        if(!this.nome || this.nome.trim().length == 0){
+            falha('Precisa de um nome')
+        }
+        if(this.preco <= 0) {
+            falha('Preço inválido')
+        }
+    }
+}
+produto.validarProduto();
+
+// // Null
+// type Contato = {
+//     nome: string,
+//     tel1: string,
+//     tel2: string | null
+// }
+
+// const contato1 : Contato = {
+//     nome: "Pablo",
+//     tel1: '9999999',
+//     tel2: null
+// }
+// console.log(contato1.nome)
+// console.log(contato1.tel1)
+// console.log(contato1.tel2)
+
+
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+}
+ 
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+ 
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
+
