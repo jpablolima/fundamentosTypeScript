e140969d0b5dd09df41dadb38306c3cebb83539e
jpablolima <jp.pablolima@gmail.com>
2020-12-11 11:56:28 -0300

    Decorators

---
+++ Decorators/decorator.ts
@@ -0,0 +1,12 @@
+@logarClasse
+class Eletrodomestico {
+    constructor(){
+        console.log('running...')
+    }
+}
+
+function logarClasse(constructor: Function){
+        console.log(constructor)
+    
+}
+
